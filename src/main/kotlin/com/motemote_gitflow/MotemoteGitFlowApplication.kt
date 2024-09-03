df10656ac9088b5e695a5b4f151796027ed7821a
engh0205@naver.com <engh0205@naver.com>
2024-09-03 15:31:15 +0900

    first commit
    깃플로우 부수기

---
+++ src/main/kotlin/com/motemote_gitflow/MotemoteGitFlowApplication.kt
@@ -0,0 +1,11 @@
+package com.motemote_gitflow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MotemoteGitFlowApplication
+
+fun main(args: Array<String>) {
+    runApplication<MotemoteGitFlowApplication>(*args)
+}
---
+++ src/test/kotlin/com/motemote_gitflow/MotemoteGitFlowApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.motemote_gitflow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MotemoteGitFlowApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
