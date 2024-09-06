a3cb48f68f5499498ea9b84986c62aa0a03dc21d
engh0205@naver.com <engh0205@naver.com>
2024-09-06 18:38:49 +0900

    Jooq 라이브러리 관련 빌드 파일 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,53 @@
+import nu.studer.gradle.jooq.JooqEdition
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
     id("org.springframework.boot") version "3.3.3"
     id("io.spring.dependency-management") version "1.1.6"
-    kotlin("plugin.jpa") version "1.9.25"
+    id("nu.studer.jooq") version "9.0"
 }
+
+jooq {
+    version.set("3.18.10")
+    edition.set(JooqEdition.OSS)
+
+    configurations {
+        create("main") {
+            generateSchemaSourceOnCompilation.set(true)
+            jooqConfiguration.apply {
+                logging = org.jooq.meta.jaxb.Logging.WARN
+                jdbc.apply {
+                    driver = "com.mysql.cj.jdbc.Driver"
+                    url = System.getenv("DB_URL") ?: "jdbc:mysql://localhost:3306/motemote"
+                    user = System.getenv("DB_USER") ?: "dooho"
+                    password = System.getenv("DB_PASSWORD") ?: "1234"
+                }
+                generator.apply {
+                    name = "org.jooq.codegen.KotlinGenerator"
+                    database.apply {
+                        name = "org.jooq.meta.mysql.MySQLDatabase"
+                        excludes = "sys"
+                        inputSchema = "motemote"
+                    }
+                    generate.apply {
+                        isDeprecated = false
+                        isFluentSetters = true
+                        isRecords = true
+                    }
+                    target.apply {
+                        packageName = "com.motemote_gitflow"
+                        directory = "build/generated-src/jooq/main"
+                    }
+                    strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+                }
+            }
+        }
+    }
+}
+
 
 group = "com"
 version = "0.0.1-SNAPSHOT"
@@ -27,16 +70,22 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-jooq")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+//    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.mysql:mysql-connector-j")
     annotationProcessor("org.projectlombok:lombok")
+    jooqGenerator("com.mysql:mysql-connector-j")
+    jooqGenerator("org.jooq:jooq-meta:3.18.10")
+    jooqGenerator("org.jooq:jooq-codegen:3.18.10")
+
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.security:spring-security-test")
@@ -49,6 +98,16 @@ kotlin {
     }
 }
 
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "21"
+        kotlinDaemonJvmArguments = listOf("-Xmx4096m", "-Xms2560m", "-XX:+UseParallelGC")
+    }
+}
+
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
