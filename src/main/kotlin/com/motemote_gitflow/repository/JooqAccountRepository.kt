4ec1a734991131a8dfec7af577d4356d10dd1f4e
engh0205@naver.com <engh0205@naver.com>
2024-09-09 17:17:08 +0900

    feat: 쿼리 - 비즈니스로직 분리
    Service Layer에서 작업하던 쿼리를 JooqRepository로 작업 위임

--- src/main/kotlin/com/motemote_gitflow/repository/JooqAccountRepository.kt
+++ src/main/kotlin/com/motemote_gitflow/repository/JooqAccountRepository.kt
@@ -1,8 +1,9 @@
 package com.motemote_gitflow.repository
 
+import com.motemote_gitflow.dto.AccountDto
 import org.springframework.stereotype.Repository
 
 @Repository
 interface JooqAccountRepository {
-
+    fun findByEmail(email : String) : AccountDto
 }
